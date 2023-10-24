88745ce72289464c26c02bdde2ed188a150227f5
Austin Rooks <Austionian@users.noreply.github.com>
2023-10-24 13:51:54 -0500

    init <

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,12 @@
 // Uncomment this block to pass the first stage
-use std::net::TcpListener;
+use std::{
+    io::Write,
+    net::{TcpListener, TcpStream},
+};
+
+fn handle_client(mut stream: TcpStream) {
+    let _ = stream.write_all(b"HTTP/1.1 200 OK\r\n\r\n");
+}
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -11,7 +18,8 @@ fn main() {
 
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
+            Ok(stream) => {
+                handle_client(stream);
                 println!("accepted new connection");
             }
             Err(e) => {
